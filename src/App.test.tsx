5e7de922981c2815118bd34b3f18613f994c3ae7
RGV <ricardo.guzman.velasco@outlook.com>
2024-01-06 18:14:46 +0100

    feat: be calm

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('wtfs empiezan en cero', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/WTFs por minuto 0/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//[ ] Por qué se pinta el botón en el centro?
+//[ ] Como hacer que reaccione react a lo que hacemos
+//[ ] Por qué no se actualiza el número de WTFs?
+
+
+var wtf = 0;
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          WTFs por minuto {wtf}
         </p>
+        <button onClick={() => wtf = wtf +1}>
+          WTF
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
