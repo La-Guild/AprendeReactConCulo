3d095085e83c9f83efa68c980300fad925f18270
RGV <ricardo.guzman.velasco@outlook.com>
2024-01-06 19:01:37 +0100

    feat: panic

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,52 @@ import './App.css';
 //[ ] Por qué se pinta el botón en el centro?
 //[ ] Como hacer que reaccione react a lo que hacemos
 //[ ] Por qué no se actualiza el número de WTFs?
+//[ ] Que es redux?
+//[ ] Dispatch?
+//[ ] Cómo se saca la consola?
+//[ ] Como depurar
+//[ ]my-5 mx-3? Esto lo mismo es de un video de youtube random y ya
 
 
-var wtf = 0;
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          WTFs por minuto {wtf}
-        </p>
-        <button className="boton" onClick={() => wtf = wtf +1}>
-          WTF!
-        </button>
-      </header>
-    </div>
-  );
+class WTF {
+    wtf: number;
+  constructor() {
+    this.wtf = 0;
+  }
+  get() {
+    return this.wtf;
+  }
+  increment() {
+    this.wtf = this.wtf + 1;
+  }
+
+
+}
+
+class Apsds extends React.Component {
+    wtf: WTF;
+    constructor(props: any) {
+        super(props);
+        this.wtf = new WTF();
+    }
+
+}
+
+function App(){
+    const [wtfs, setWtfs] = React.useState(0);
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo"/>
+            <p>
+              WTFs por minuto {wtfs}
+            </p>
+            <button className="boton" onClick={() => { setWtfs(wtfs + 1)}}>
+              WTF!
+            </button>
+          </header>
+        </div>
+    );
 }
 
 export default App;
