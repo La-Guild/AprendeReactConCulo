042f35602e9937b32086cd6f05c6c5a12a2b331d
RGV <ricardo.guzman.velasco@outlook.com>
2024-01-07 10:49:10 +0100

    test: rename stuff

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,16 +4,16 @@ import App from './App';
 
 test('wtfs empiezan en cero', () => {
   render(<App />);
-  const linkElement = screen.getByText(/WTFs por minuto 0/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = screen.getByText(/WTFs por minuto 0/i);
+  expect(text).toBeInTheDocument();
 });
 
 test('wtfs aumentan', () => {
   render(<App />);
   const button = screen.getByText(/WTF!/i);
   fireEvent.click(button)
-  const texto = screen.getByText(/WTFs por minuto 1/i);
-  expect(texto).toBeInTheDocument();
+  const text = screen.getByText(/WTFs por minuto 1/i);
+  expect(text).toBeInTheDocument();
 });
 
 
